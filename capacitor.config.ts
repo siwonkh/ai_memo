4384c404508dedd6c5975d76820a3d15f2a36ee5
siwonkh <kanghyunkim313@gmail.com>
2024-11-07 23:24:37 +0900

    chore: add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.siwonkh.ai_memo',
+  appName: 'ai-memo',
+  webDir: 'dist'
+};
+
+export default config;
