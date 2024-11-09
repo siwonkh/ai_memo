06f395f6fe66044c91a6bfc50deb20570c29cddb
siwonkh <kanghyunkim313@gmail.com>
2024-11-09 19:01:57 +0900

    fix(todo): change from firestore/lite to firestore

--- src/lib/todo.ts
+++ src/lib/todo.ts
@@ -5,7 +5,7 @@ import {
 	query,
 	where,
 	type Firestore
-} from "firebase/firestore/lite";
+} from "firebase/firestore";
 import type { User } from "firebase/auth";
 
 export const getTodos = async (serverDB: Firestore, user: User) => {
@@ -13,7 +13,7 @@ export const getTodos = async (serverDB: Firestore, user: User) => {
 		query(
 			collection(serverDB, 'todos'),
 			where('uid', '==', user.uid),
-			orderBy('createdAt', 'desc')
+			orderBy('createdAt')
 		)
 	);
 
